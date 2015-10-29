8b38df3eed1b23a93904818620582404dded9da6
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-10-29 22:44:26 +0000

    Start

---
+++ lib/META6.pm
@@ -0,0 +1,36 @@
+use v6;
+
+use JSON::Fast;
+
+class META6 {
+
+    class Resource {
+
+    }
+    class Support {
+
+    }
+
+    has Version     $.perl          is rw;
+    has Str         $.name          is rw;
+    has Version     $.version       is rw;
+    has Str         $.description   is rw;
+    has Str         @.authors       is rw;
+    has Str         %.provides      is rw;
+    has Str         @.depends       is rw;
+    has Str         %.emulates      is rw;
+    has Str         %.supersedes    is rw;
+    has Str         %.superseded_by is rw;
+    has Str         %.excludes      is rw;
+    has Str         @.build-depends is rw;
+    has Str         @.test-depends  is rw;
+    has Resource    $.resource      is rw;
+    has Support     $.support       is rw;
+    has Bool        $.production    is rw;
+    has Str         $.license       is rw;
+    has Str         @.tags          is rw;
+
+
+}
+
+# vim: expandtab shiftwidth=4 ft=perl6
