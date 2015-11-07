732526906bfd291bfff9ae0dc6358bebfa7df59f
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-11-07 15:36:51 +0000

    Add the traits

--- lib/META6.pm
+++ lib/META6.pm
@@ -8,6 +8,26 @@ import JSON::Unmarshal;
 
 class META6 does JSON::Class {
 
+    enum Optionality <Mandatory Optional>;
+
+    role MetaAttribute {
+        has Optionality $.optionality is rw;
+    }
+
+    role MetaAttribute::Specfication does MetaAttribute {
+
+    }
+
+    role MetaAttribute::Customary does MetaAttribute {
+
+    }
+
+    multi sub trait_mod:<is> (Attribute $a, Optionality :$specification!) {
+
+        $a does MetaAttribute::Specfication;
+        $a.optionality = $specification // Optional;
+    }
+
     multi method new(Str :$file!) {
         self.new(file => $file.IO);
     }
@@ -38,25 +58,24 @@ class META6 does JSON::Class {
     }
 
 
-    has Version     $.perl          is rw is marshalled-by('Str') is unmarshalled-by(&unmarsh-version);
-    has Str         $.name          is rw;
-    has Version     $.version       is rw is marshalled-by('Str') is unmarshalled-by(&unmarsh-version);
-    has Str         $.description   is rw;
-    has Str         @.authors       is rw;
-    has Str         %.provides      is rw;
-    has Str         @.depends       is rw;
-    has Str         %.emulates      is rw;
-    has Str         %.supersedes    is rw;
-    has Str         %.superseded-by is rw;
-    has Str         %.excludes      is rw;
-    has Str         @.build-depends is rw;
-    has Str         @.test-depends  is rw;
-    has Resource    $.resource      is rw;
-    has Support     $.support       is rw;
-    has Bool        $.production    is rw;
-    has Str         $.license       is rw;
-    has Str         @.tags          is rw;
-
+    has Version     $.perl          is rw is marshalled-by('Str') is unmarshalled-by(&unmarsh-version) is specification(Mandatory);
+    has Str         $.name          is rw is specification(Mandatory);
+    has Version     $.version       is rw is marshalled-by('Str') is unmarshalled-by(&unmarsh-version) is specification(Mandatory);
+    has Str         $.description   is rw is specification(Mandatory);
+    has Str         @.authors       is rw is specification(Optional);
+    has Str         %.provides      is rw is specification(Mandatory);
+    has Str         @.depends       is rw is specification(Optional);
+    has Str         %.emulates      is rw is specification(Optional);
+    has Str         %.supersedes    is rw is specification(Optional);
+    has Str         %.superseded-by is rw is specification(Optional);
+    has Str         %.excludes      is rw is specification(Optional);
+    has Str         @.build-depends is rw is specification(Optional);
+    has Str         @.test-depends  is rw is specification(Optional);
+    has Resource    $.resource      is rw is specification(Optional);
+    has Support     $.support       is rw is specification(Optional);
+    has Bool        $.production    is rw is specification(Optional);
+    has Str         $.license       is rw is specification(Optional);
+    has Str         @.tags          is rw is specification(Optional);
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6
